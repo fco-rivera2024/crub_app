09da290499967c986636fa99e7343056ed907893
fco_rivera <fco.rivera.castillo.c2@gmail.com>
2024-12-03 07:31:57 -0300

    Archivos Iniciales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ProductosComponent } from "./Productos/productos.component" ;
+import { ServiciosComponent } from './Servicios/servicios.component';
+import { ContactosComponent } from './contactos/contactos.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+    {path: "Productos", component:ProductosComponent},
+    {path: "Servicios", component:ServiciosComponent},
+    {path: "Contactos", component:ContactosComponent},
+];
